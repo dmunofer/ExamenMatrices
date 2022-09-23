7cc86dd4e44ec06c5cb0a5a64aae61cb39106f8a
dmunofer <diego.fmz@hotmail.com>
2022-09-23 16:06:19 +0200

    intento iterativo

---
+++ Ejercicio1.py
@@ -0,0 +1,11 @@
+
+
+def ejercicio1(matriz):
+
+    for e in range(len(matriz)):
+        if matriz[e][3]==sum(list((str(e[0]),str(e[1]),str(e[2])))):
+            pass
+        else:
+            matriz[e][3]=sum(list((str(e[0]),str(e[1]),str(e[2]))))
+
+    return matriz
