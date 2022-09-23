e054173a80de00951acd029b7bc096feb76ecc39
dmunofer <diego.fmz@hotmail.com>
2022-09-23 16:18:08 +0200

    ejercicio 2 arreglado y terminado

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,7 +1,7 @@
 
 def ejercicio2():
     cadena= input('Introduce una cadena de caracteres:')
-    if len(cadena)<=3 and len(cadena)<10:
+    if len(cadena)>=3 and len(cadena)<10:
         return True
     else:
         return False
