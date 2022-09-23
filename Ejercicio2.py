93c5817017ebe532e2cbad9bd8d78ac3a4d4f03a
dmunofer <diego.fmz@hotmail.com>
2022-09-23 16:36:33 +0200

    ejercicio2 rec mal

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -13,5 +13,9 @@ def ejercicio2_rec():
     final = True
     if len(cadena)<3 or cont>3:
         final = False
+        list(cadena).remove(cadena[0])
+        list(cadena).remove(cadena[1])
+        list(cadena).remove(cadena[2])
+        str(cadena)
     return ejercicio2_rec(), final
     
