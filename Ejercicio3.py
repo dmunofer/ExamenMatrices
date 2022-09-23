937220513f839b5e939f9acfd00d4bd276007d67
dmunofer <diego.fmz@hotmail.com>
2022-09-23 16:44:19 +0200

    ejercicio3 sin recursion :(

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -6,3 +6,33 @@ def del_1_al_10():
         lista.append(i)
     return lista
 
+
+def del_menos10_al_0():
+    lista=[]
+    for i in range(-10,1):
+        lista.append(i)
+    return lista
+
+def pares0_al_10():
+    lista=[]
+    for i in range(0,21,2):
+        lista.append(i)
+    return lista
+
+def del_menos20_al_0_impares():
+    lista=[]
+    for i in range(-21,0,2):
+        lista.append(i)
+    lista.remove(lista[0])
+    return lista
+
+def del_0_al_50_multiples():
+    lista=[]
+    for i in range(0,51,5):
+        lista.append(i)
+    return lista
+
+
+
+
+
