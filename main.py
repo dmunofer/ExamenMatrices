30b8f63aa3795d9f402829ab10a8afe28054c855
dmunofer <diego.fmz@hotmail.com>
2022-09-23 17:21:12 +0200

    main hasta el 3

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
+from tkinter import E
 import Ejercicio1
 import Ejercicio2
 import Ejercicio3
 import tabla
+
+
+matriz = [
+
+    [1, 1, 1, 3],
+
+    [2, 2, 2, 7],
+
+    [3, 3, 3, 9],
+
+    [4, 4, 4, 13]
+
+]
+
+Ejercicio1.ejercicio1(matriz)
+Ejercicio2.ejercicio2()
+Ejercicio3.del_1_al_10()
+Ejercicio3.del_menos10_al_0()
+Ejercicio3.pares0_al_10()
+Ejercicio3.del_menos20_al_0_impares()
+Ejercicio3.del_0_al_50_multiples()
