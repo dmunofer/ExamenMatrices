7cbc60c65f99370c3c30b705b2d6344e61fb2d92
dmunofer <diego.fmz@hotmail.com>
2022-09-23 16:16:35 +0200

    ejercicio 1 hecho e intento del 2

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -3,9 +3,9 @@
 def ejercicio1(matriz):
 
     for e in range(len(matriz)):
-        if matriz[e][3]==sum(list((str(e[0]),str(e[1]),str(e[2])))):
+        if matriz[e][3]==sum((matriz[e][0],matriz[e][1],matriz[e][2])):
             pass
         else:
-            matriz[e][3]=sum(list((str(e[0]),str(e[1]),str(e[2]))))
+            matriz[e][3]=sum((matriz[e][0],matriz[e][1],matriz[e][2]))
 
     return matriz
